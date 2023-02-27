f273b965f9dbfcfb3c2ad7c9ac2c67d41eef4798
ABDALRHMANWALID <102229790+ABDALRHMANWALID@users.noreply.github.com>
2023-02-27 15:38:56 -0800

    Delete projects directory

--- projects/user/src/app/shared/pipe/short-text.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShortTextPipe } from './short-text.pipe';
-
-describe('ShortTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ShortTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
